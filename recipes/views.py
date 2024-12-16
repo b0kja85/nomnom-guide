6898edda44c25114dd5cf73fd110ad4110a081b1
b0kja85 <jkm.kiunisala@gmail.com>
2024-12-17 02:40:54 +0800

    feat(home): added pagination

--- recipes/views.py
+++ recipes/views.py
@@ -6,47 +6,48 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from . import models
 
 class RecipeListView(ListView):
-  model = models.Recipe
-  template_name = 'recipes/home.html'
-  context_object_name = 'recipes'
+    model = models.Recipe
+    template_name = 'recipes/home.html'
+    context_object_name = 'recipes'
+    paginate_by = 5  # Show 5 recipes per page
 
-# Create your views here.
-def home(request):
-  recipes = models.Recipe.objects.all()
-  context = {
-    'recipes': recipes
-  }
-  return render(request, 'recipes/home.html', context)
-
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        paginator = context['paginator']
+        page_obj = paginator.get_page(self.request.GET.get('page'))
+        
+        context['page_obj'] = page_obj
+        
+        return context
 
 class RecipeDetailView(DetailView):
-  model = models.Recipe
+    model = models.Recipe
 
 class RecipeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-  model = models.Recipe
-  success_url = reverse_lazy('recipes-home')
+    model = models.Recipe
+    success_url = reverse_lazy('recipes-home')
 
-  def test_func(self):
-    recipe = self.get_object()
-    return self.request.user == recipe.author
+    def test_func(self):
+        recipe = self.get_object()
+        return self.request.user == recipe.author
 
 class RecipeCreateView(LoginRequiredMixin, CreateView):
-  model = models.Recipe
-  fields = ['title', 'description']
+    model = models.Recipe
+    fields = ['title', 'description']
 
-  def form_valid(self, form):
-    form.instance.author = self.request.user
-    return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class RecipeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-  model = models.Recipe
-  fields = ['title', 'description']
+    model = models.Recipe
+    fields = ['title', 'description']
 
-  def test_func(self):
-    recipe = self.get_object()
-    return self.request.user == recipe.author
+    def test_func(self):
+        recipe = self.get_object()
+        return self.request.user == recipe.author
 
-  def form_valid(self, form):
-    form.instance.author = self.request.user
-    return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
