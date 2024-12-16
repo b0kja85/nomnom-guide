3635529bd3ba0fa4fc4f372002f40a03a3115eb6
b0kja85 <jkm.kiunisala@gmail.com>
2024-12-17 05:57:41 +0800

    feat: show all recipes in the profile

--- config/urls.py
+++ config/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     # new urls
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="user-login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="user-logout"),
-    path('profile/', user_views.profile, name="user-profile"),
+    path('profile/', user_views.profile, name="user-profile")
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from recipes.models import Recipe
 from . import forms
 
 # Create your views here.
@@ -21,4 +22,6 @@ def register(request):
 
 @login_required()
 def profile(request):
-  return render(request, 'users/profile.html')
+  # Filter recipes by the logged-in user
+  recipes = Recipe.objects.filter(author=request.user)
+  return render(request, 'users/profile.html', {"recipes": recipes})
