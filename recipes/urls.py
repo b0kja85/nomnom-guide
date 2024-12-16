c7463cf0501befac650603156882585937893712
vicicoed <vinjove323@gmail.com>
2024-12-17 01:31:50 +0800

    About page removed

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('recipe/create', views.RecipeCreateView.as_view(), name="recipes-create"),
     path('recipe/<int:pk>/update', views.RecipeUpdateView.as_view(), name="recipes-update"),
     path('recipe/<int:pk>/delete', views.RecipeDeleteView.as_view(), name="recipes-delete"),
-    path('about/', views.about, name="recipes-about"),
 ]
